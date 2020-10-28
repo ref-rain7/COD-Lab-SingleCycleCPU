37d12c76ad3ac2228877a06376fab0bb3235753d
zero <zero@smail.nju.edu.cn>
2020-10-28 13:27:35 +0800

    add code in main to generate verilog

--- src/test/scala/riscv/main.scala
+++ src/test/scala/riscv/main.scala
@@ -1,10 +1,14 @@
 package riscv
 
-import chisel3.stage.ChiselStage
+import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 object main {
     def main(args: Array[String]) {
         //println((new ChiselStage).emitVerilog(new RegisterFile))
+        (new ChiselStage).execute(
+            Array("-X", "verilog"),
+            Seq(ChiselGeneratorAnnotation(() => new SingleCycleCPU))
+        )
     }
 }
